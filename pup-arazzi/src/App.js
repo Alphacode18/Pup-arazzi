98a7c85bbaff3ed6621ebf9c381fd133078beb1a
Alphacode18 <skharban@purdue.edu>
2020-07-22 17:42:26 +0530

    Routing Added

--- pup-arazzi/src/App.js
+++ pup-arazzi/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- pup-arazzi/src/components/layout/Navbar.js
+++ pup-arazzi/src/components/layout/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
   return (
     <nav className='navbar bg-success'>
       <h1>Pup-arazzi</h1>
+      <ul>
+        <li>
+          <Link to='/about' />
+          About
+        </li>
+      </ul>
     </nav>
   );
 };
