ac552f4f903025e1a760d629d06c7ba4e352b304
Alphacode18 <skharban@purdue.edu>
2020-07-22 16:57:37 +0530

    Navbar constructed

---
+++ pup-arazzi/src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = (props) => {
+  return (
+    <nav className='navbar bg-success'>
+      <h1>Pup-arazzi</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
